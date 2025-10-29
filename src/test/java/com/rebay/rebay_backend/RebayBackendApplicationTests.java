5321a2fac4f87270eabb67cf2b84549057c92cf1
woon <woon7713@gmail.com>
2025-10-29 16:49:29 +0900

    settings: docker-compose

---
+++ src/test/java/com/rebay/rebay_backend/RebayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebay.rebay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
