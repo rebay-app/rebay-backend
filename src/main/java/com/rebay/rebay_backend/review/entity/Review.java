07bf6331fb43085acabea3863faf8e1328432633
m131y <whalsdud0131@gmail.com>
2025-11-08 22:37:44 +0900

    feat(review): 리뷰 관련 API

---
+++ src/main/java/com/rebay/rebay_backend/review/entity/Review.java
@@ -0,0 +1,48 @@
+package com.rebay.rebay_backend.review.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.rebay.rebay_backend.Post.entity.Post;
+import com.rebay.rebay_backend.payment.entity.Transaction;
+import com.rebay.rebay_backend.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reviewer_id", nullable = false)
+    @JsonBackReference
+    private User reviewer;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction_id", unique = true, nullable = false)
+    private Transaction transaction;
+
+    private String content;
+    @Column(nullable = false)
+    private StarRating rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
